13937bc1dc36d70a0f042d11b8b0928f90640298
Dominic Cassidy <dcassidy@arago.de>
2020-09-28 10:28:54 +0100

    Add check feature

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+import * as jsdiff from "https://cdn.skypack.dev/diff@^4.0.2";
+import { red, green } from "https://deno.land/std/fmt/colors.ts";
+
 export class JFilter {
   private value = "";
 
@@ -31,6 +34,35 @@ export class JFilter {
     return `${this.value}`;
   }
 }
+
+export const diff = (actual: string, expected: string) => {
+  const diff = jsdiff.diffChars(actual, expected, undefined) as any[];
+
+  let match = true;
+
+  const output = diff.reduce((acc: string, part: any) => {
+    if (!part.value) {
+      return;
+    }
+
+    if (part.added) {
+      match = false;
+      acc += green(part.value);
+    } else if (part.removed) {
+      match = false;
+      acc += red(part.value);
+    } else {
+      acc += part.value;
+    }
+
+    return acc;
+  }, "");
+
+  return {
+    result: match,
+    output,
+  };
+};
 
 export type Action = "CREATE" | "GET" | "UPDATE" | "DELETE";
 
@@ -79,7 +111,18 @@ if (Deno.args[0]) {
   const filename = Deno.args[0];
   const data = await Deno.readTextFile(filename).then((f) => JSON.parse(f));
 
-  const res = parse(data);
+  const filter = parse(data);
 
-  console.log(res);
+  if (Deno.args[1]) {
+    const check = Deno.args[1];
+    const res = diff(filter, check);
+
+    if (!res.result) {
+      console.log("Result did not match");
+      console.log(res.output);
+      Deno.exit(1);
+    }
+  }
+
+  console.log(filter);
 }
--- main_test.ts
+++ main_test.ts
@@ -1,5 +1,6 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import { parse, FilterData } from "./main.ts";
+
+import { parse, FilterData, diff } from "./main.ts";
 
 Deno.test("Jfilter #1", () => {
   const filter: FilterData[] = [
@@ -41,4 +42,38 @@ Deno.test("Jfilter #2", () => {
     res,
     `|(&(|(action = GET)(action = CREATE)(action = UPDATE)(action = DELETE))(vertex.ogit/_type = ogit/Mobile/*))`
   );
+});
+
+Deno.test("Jfilter #3", async () => {
+  const filter: FilterData[] = [
+    {
+      action: "GET",
+      values: {
+        "vertex.ogit/_type": [
+          "ogit/Automation/KnowledgeItem",
+          "ogit/Automation/AutomationIssue",
+          "ogit/Automation/KnowledgePool",
+        ],
+      },
+    },
+    {
+      action: ["GET", "UPDATE", "CREATE"],
+      values: {
+        "vertex.ogit/_type": [
+          "ogit/Timeseries",
+          "ogit/Automation/AutomationIssue",
+          "ogit/Knowledge/AcquisitionSession",
+        ],
+      },
+    },
+  ];
+
+  const res = parse(filter);
+
+  const d = diff(
+    res,
+    `|(&(action = GET)(|(vertex.ogit/_type = ogit/Automation/KnowledgeItem)(vertex.ogit/_type = ogit/Automation/AutomationIssue)(vertex.ogit/_type = ogit/Automation/KnowledgePool)))(&(|(action = GET)(action = UPDATE)(action = CREATE))(|(vertex.ogit/_type = ogit/Timeseries)(vertex.ogit/_type = ogit/Knowledge/AcquisitionSession)))`
+  );
+
+  assertEquals(d.result, true, d.output);
 });
